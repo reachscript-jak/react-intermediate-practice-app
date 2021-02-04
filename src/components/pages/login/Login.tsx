3319d999abea63b9c10d01e2508945ed4949d91e
Takumi Okada <44320334+reachscript-jak@users.noreply.github.com>
2021-02-04 19:08:51 +0900

    ログイン画面の作成

--- src/components/pages/login/Login.tsx
+++ src/components/pages/login/Login.tsx
@@ -1,5 +1,34 @@
 import { memo, VFC } from "react";
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  Input,
+  Stack
+} from "@chakra-ui/react";
 
 export const Login: VFC = memo(() => {
-  return <p>Loginページです</p>;
+  return (
+    <Flex alignItems="center" justifyContent="center" height="100vh">
+      <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
+        <Heading as="h1" size="lg" textAlign="center">
+          ユーザー管理アプリ
+        </Heading>
+        <Divider my={4} />
+        <Stack spacing={6} py={4} px={10}>
+          <Input placeholder="ユーザーID" />
+          <Button
+            bg="teal.400"
+            color="white"
+            isFullWidth
+            _hover={{ opacity: 0.8 }}
+          >
+            ログイン
+          </Button>
+        </Stack>
+      </Box>
+    </Flex>
+  );
 });
